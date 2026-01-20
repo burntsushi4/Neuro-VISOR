78ac58298469b8e9f942417ebd82ad77e4b4382a
burntsushi4 <maryamamr251@gmail.com>
2026-01-20 12:51:39 -0500

    camera preset using object transform in scene

---
+++ Assets/Scripts/C2M2/Interaction/DesktopCameraPresets.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using C2M2.Utils;
+public class DesktopCameraPresets : MonoBehaviour
+{
+    public Transform preset1;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.position = preset1.position;
+            transform.rotation = preset1.rotation;
+        }
+    }
+}
